32e7b7166f172d5853edcba7c7a796d66984615e
steven rogers <me@srogers.net>
2016-07-12 22:09:54 -0400

    Added: AlamofireGloss.xcodeproj to support Carthage.

---
+++ Supporting Files/AlamofireGloss.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireGloss.h
+//  AlamofireGloss
+//
+//  Created by steven rogers on 7/12/16.
+//  Copyright © 2016 steven rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireGloss.
+FOUNDATION_EXPORT double AlamofireGlossVersionNumber;
+
+//! Project version string for AlamofireGloss.
+FOUNDATION_EXPORT const unsigned char AlamofireGlossVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireGloss/PublicHeader.h>
+
+
