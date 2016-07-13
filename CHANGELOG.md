3b465b1a9b0b81aa156068799b2a2677a8b8bcae
steven rogers <me@srogers.net>
2016-07-12 22:30:55 -0400

    Added: Basic unit tests. Added: CHANGELOG.

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+# Change Log
+`Alamofire-Gloss` adheres to [Semantic Versioning](http://semver.org/).
+
+- [1.0.0](#100)
+
+---
+
+## [1.0.0](https://github.com/spxrogers/Alamofire-Gloss/releases/tag/1.0.0)
+
+#### Initial Release
+- Provides Gloss bindings for Alamofire.
+- Supports CocoaPods + Carthage.
+- Includes a demo app and unit tests.
+
--- Demo and Tests/Tests/Tests.swift
+++ Demo and Tests/Tests/Tests.swift
@@ -1,35 +1,107 @@
-//
-//  Tests.swift
-//  Tests
-//
-//  Created by steven rogers on 7/12/16.
-//  Copyright © 2016 steven rogers. All rights reserved.
-//
 
-import XCTest
+@testable import Demo
+import Quick
+import Nimble
+import Alamofire
+import Gloss
+import Alamofire_Gloss
 
-class Tests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+class ResponseGlossSpec: QuickSpec {
+  override func spec() {
+    let personUrl = "http://srogers.net/rest/person.json"
+    let peopleUrl = "http://srogers.net/rest/people.json"
+    let badFormatUrl = "http://srogers.net/rest/bad_format.json"
+    let badPersonUrl = "http://srogers.net/rest/bad_person.json"
+  
+    // standard
+    it("handles a core object request") {
+      let steven = Person(json: ["name": "steven rogers", "age": 21])!
+      var equal = false
+      
+      waitUntil(timeout: 5) { done in
+        Alamofire.request(.GET, personUrl, parameters: nil).responseObject(Person.self) { (response) in
+          switch response.result {
+          case .Success(let person):
+            equal = steven == person
+          case .Failure(_):
+            equal = false
+          }
+          done()
         }
+      }
+      expect(equal).to(beTruthy())
     }
     
+    it("handles a core array request") {
+      let steven = Person(json: ["name": "steven rogers", "age": 21])!
+      let john = Person(json: ["name": "john doe"])!
+      let people = [steven, john]
+      var equal = false
+      
+      waitUntil(timeout: 5) { done in
+        Alamofire.request(.GET, peopleUrl, parameters: nil).responseArray(Person.self) { (response) in
+          switch response.result {
+          case .Success(let responsePeople):
+            equal = people == responsePeople
+          case .Failure(_):
+            equal = false
+          }
+          done()
+        }
+      }
+      expect(equal).to(beTruthy())
+    }
+    
+    // bad requests
+    it("handles a core object missing required gloss fields") {
+      var failedWhenExpected = false
+      
+      waitUntil(timeout: 5) { done in
+        Alamofire.request(.GET, badPersonUrl, parameters: nil).responseObject(Person.self) { (response) in
+          switch response.result {
+          case .Success(_):
+            failedWhenExpected = false
+          case .Failure(_):
+            failedWhenExpected = true
+          }
+          done()
+        }
+      }
+      expect(failedWhenExpected).to(beTruthy())
+    }
+    
+    it("handles a core object invalid format") {
+      var failedWhenExpected = false
+      
+      waitUntil(timeout: 5) { done in
+        Alamofire.request(.GET, badFormatUrl, parameters: nil).responseObject(Person.self) { (response) in
+          switch response.result {
+          case .Success(_):
+            failedWhenExpected = false
+          case .Failure(_):
+            failedWhenExpected = true
+          }
+          done()
+        }
+      }
+      expect(failedWhenExpected).to(beTruthy())
+    }
+    
+    it("handles a core array invalid format") {
+      var failedWhenExpected = false
+      
+      waitUntil(timeout: 5) { done in
+        Alamofire.request(.GET, badPersonUrl, parameters: nil).responseArray(Person.self) { (response) in
+          switch response.result {
+          case .Success(_):
+            failedWhenExpected = false
+          case .Failure(_):
+            failedWhenExpected = true
+          }
+          done()
+        }
+      }
+      expect(failedWhenExpected).to(beTruthy())
+    }
+  }
 }
